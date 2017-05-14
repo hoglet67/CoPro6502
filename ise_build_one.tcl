f1689e592ff1ec05437f070b9b1921d09358ad61
David Banks <dave@hoglet.com>
2017-05-14 07:10:32 +0100

    80x86 Co Pro: Reduced speed from 16MHz to 8MHz
    Change-Id: I056d3cc927702fd438214e30eb5a5610c0aaeed2

---
+++ ise_build_one.tcl
@@ -0,0 +1,9 @@
+#!/opt/Xilinx/14.7/ISE_DS/ISE/bin/lin/xtclsh
+project open LX9Co-x86.xise
+project clean
+project close
+project open LX9Co-x86.xise
+process run "Generate Programming File"
+project close
+exit
+
